4a0726c683c6dcd10fd5356294d58af6ca6abbf3
Djóni Frank Strømsten <djonifrank@gmail.com>
2020-08-27 20:16:40 +0100

    added barebones functionallity

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(['Take dogs for a walk', 'Take out trash']);
+  const [input, setInput] = useState('');
+
+  const addTodo = event => {
+    event.preventDefault();
+    setTodos([...todos, input]);
+    setInput('');
+  }
+
   return (
     <div className="app">
-      <input />
-      <button>Add Todo</button>
+      <form>
+        <input value={input} onChange={event => setInput(event.target.value)} />
+        <button type='submit' onClick={addTodo}>Add ToDo</button>
+      </form>
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
