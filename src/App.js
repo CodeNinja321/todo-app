d17201f73fbc612ff29b716ab7a1c04cd30981ce
Djóni Frank Strømsten <djonifrank@gmail.com>
2020-08-27 22:38:00 +0100

    added db functionallity

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, FormControl, InputLabel, FormHelperText, Input } from '@material-ui/core';
 import './App.css';
 import Todo from './Todo';
+import db from './firebase';
+import firebase from 'firebase';
 
 function App() {
-  const [todos, setTodos] = useState(['Take dogs for a walk', 'Take out trash']);
+  const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('');
+
+  useEffect(() => {
+    db.collection('todos').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
+      setTodos(snapshot.docs.map(doc => doc.data().todo));
+    })
+  }, []);
 
   const addTodo = event => {
     event.preventDefault();
-    setTodos([...todos, input]);
+    db.collection('todos').add({
+        todo: input,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    });
     setInput('');
   }
 
